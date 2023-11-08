251546776e5a49dc851111dd914b7cab63f474f9
@vamsi753 <ltybjy666@gmail.com>
2023-11-08 15:05:18 +0530

    first commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace YourConsoleAppNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        static void runApp()
+        {
+            
+            string[] studentNames = new string[] { "Alice", "Bob", "Charlie", "David", "Eve" };
+            int[] studentMarks = new int[] { 90, 85, 78, 92, 88 };
+
+            Console.WriteLine("List of Students and Their Marks:");
+            for (int i = 0; i < studentNames.Length; i++)
+            {
+                Console.WriteLine($"{studentNames[i]} - Mark: {studentMarks[i]}");
+            }
+
+            int totalMarks = 0;
+            for (int i = 0; i < studentMarks.Length; i++)
+            {
+                totalMarks += studentMarks[i];
+            }
+            double averageMark = (double)totalMarks / studentMarks.Length;
+
+            Console.WriteLine($"Average Mark: {averageMark:F2}");
+        }
+    }
+}
